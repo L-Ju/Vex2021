00a736afa92384afbb90b5f64b1d030d0e976cb3
Lu Ju <luju031101@outlook.com>
2021-02-03 16:17:07 +1030

    globals

---
+++ .vscode/cquery_cached_index/@Users@luju@Desktop@vex2021@Vex2021/src@globals.cpp
@@ -0,0 +1,36 @@
+#include "main.h"
+
+// controllers
+Controller controller;
+ControllerButton rollerLeftBotton(ControllerDigital::A);
+ControllerButton rollerRightBotton(ControllerDigital::B);
+
+// drive motors
+int FRONT_LEFT_MOTOR_PORT  =  13;
+int FRONT_RIGHT_MOTOR_PORT  =  18;
+int BACK_LEFT_MOTOR_PORT  =  14;
+int BACK_RIGHT_MOTOR_PORT  =  17;
+Motor MTR_frontLeft(FRONT_LEFT_MOTOR_PORT);
+Motor MTR_frontRight(FRONT_RIGHT_MOTOR_PORT);
+Motor MTR_backLeft(BACK_LEFT_MOTOR_PORT);
+Motor MTR_backRight(BACK_RIGHT_MOTOR_PORT);
+
+// roller motors
+int LEFT_ROLLER_MOTOR_PORT = 11;
+int RIGHT_ROLLER_MOTOR_PORT = 20;
+Motor MTR_rollerLeft(LEFT_ROLLER_MOTOR_PORT);
+Motor MTR_rollerRight(RIGHT_ROLLER_MOTOR_PORT);
+
+// shooter
+int SHOOTER_MOTOR_PORT = 15;
+Motor MTR_shooter(SHOOTER_MOTOR_PORT);
+
+// pushup
+int PUSHUP_MOTOR_PORT = 12;
+Motor MTR_pushup(PUSHUP_MOTOR_PORT);
+
+// chassis
+std::shared_ptr<ChassisController> drive =
+ChassisControllerBuilder()
+  .withMotors({FRONT_LEFT_MOTOR_PORT, BACK_LEFT_MOTOR_PORT},{FRONT_RIGHT_MOTOR_PORT, BACK_RIGHT_MOTOR_PORT})
+  .withDimensions(AbstractMotor::gearset::green, {{4_in, 11.5_in}, imev5GreenTPR}).build();
