84b8d4f38d23711bb1399b268396c9c4505a05e6
Lu Ju <luju031101@outlook.com>
2021-02-24 08:55:38 +1030

    changes1

---
+++ .vscode/cquery_cached_index/@Users@luju@Desktop@vex2021@Vex2021/src@globals.hpp
@@ -0,0 +1,37 @@
+#include "main.h"
+
+// controllers
+extern Controller controller;
+
+extern double ticksPerInch;
+extern double kP;
+extern double kI;
+extern double kD;
+extern int autoRoutine;
+
+// drive motors
+extern int FRONT_LEFT_MOTOR_PORT;
+extern int FRONT_RIGHT_MOTOR_PORT;
+extern int BACK_LEFT_MOTOR_PORT;
+extern int BACK_RIGHT_MOTOR_PORT;
+extern Motor MTR_frontLeft;
+extern Motor MTR_frontRight;
+extern Motor MTR_backLeft;
+extern Motor MTR_backRight;
+
+// roller motors
+extern int LEFT_ROLLER_MOTOR_PORT;
+extern int RIGHT_ROLLER_MOTOR_PORT;
+extern Motor MTR_rollerLeft;
+extern Motor MTR_rollerRight;
+
+// shooter
+extern int SHOOTER_MOTOR_PORT;
+extern Motor MTR_shooter;
+
+// pushup
+extern int PUSHUP_MOTOR_PORT;
+extern Motor MTR_pushup;
+
+// chassis
+extern std::shared_ptr<ChassisController> drive;
