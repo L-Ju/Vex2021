c02a7313bc198016d4e66f81a3b886fd4c421ed1
Emil Smith <smithomaster@gmail.com>
2021-05-16 14:55:49 +0930

    cock

--- src/main.cpp
+++ src/main.cpp
@@ -233,6 +233,8 @@ void autonomous() {
     MTR_shooter.moveVelocity(600);
     pros::delay(500);
     MTR_shooter.moveVelocity(0);
+    pros::Motor MTR_5(SHOOTER_MOTOR_PORT);
+    MTR_5.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
     drive->moveDistance(80_in);
 }
 
